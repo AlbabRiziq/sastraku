21f53f14079c00aa54abbca395e69b8db1aa4d06
AlbabRiziq <albabriziq123@gmail.com>
2024-03-13 07:24:27 +0700

    Delete User model and update signup route

--- web_app/src/app/api/signup/route.ts
+++ web_app/src/app/api/signup/route.ts
@@ -1,10 +1,26 @@
 import mongoose from "mongoose";
 import dbConnect from "../../lib/dbConnect";
-import User from "@/Models/User";
+import User from "../../../Models/User";
+import validator from "validator";
 
 export async function POST(req: Request, res: Response) {
-  await dbConnect();
 
+  await dbConnect()
+  
+  const {searchParams} = new URL(req.url)
+  const namaLengkap = searchParams.get("nama_lengkap")
+  const username = searchParams.get("username")
+  const passwd = searchParams.get("password")
+
+  const validatePasswd = validator.isLength(passwd, { min: 5 })
+  
+  const userN = User.findOne({
+    username: username
+  }).exec()
+
+  console.log(await userN);
+  
+  
   
 
   return Response.json({ message: "asd" });
