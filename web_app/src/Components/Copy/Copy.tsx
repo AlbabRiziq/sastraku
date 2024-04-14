a5e7a2a6e322608239987bc1e2347e7c0790bde5
AlbabRiziq <albabriziq123@gmail.com>
2024-04-15 00:56:19 +0700

    Fix button onClick event in Copy.tsx

--- web_app/src/Components/Copy/Copy.tsx
+++ web_app/src/Components/Copy/Copy.tsx
@@ -2,7 +2,7 @@ function Copy({ title }) {
 
 
     return (
-        <button className="btn text-xs px-12 bg-[#092635] text-[#9ec8ba] mt-5" onClick={handleClick}>
+        <button className="btn text-xs px-12 bg-[#092635] text-[#9ec8ba] mt-5" >
             {title}
         </button>
     );
