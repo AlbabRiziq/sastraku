d6ac9043b0e0e8f075f537745a465fdff063606f
AlbabRiziq <albabriziq123@gmail.com>
2024-04-17 04:16:25 +0700

    Refactor Card component to improve layout and add image prop

--- web_app/src/app/page.tsx
+++ web_app/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   useEffect(() => {
     axios({
       method: 'get',
-      url: '/api/recommend',
+      url: 'https://sastraku.vercel.app/api/recommend',
       headers: {
         'Content-Type': 'application/json'
       }
@@ -33,7 +33,7 @@ export default function Home() {
       <div className="carousel w-full px-5">
         <h1 className="mt-5 font font-bold text-xl">REKOMENDASI</h1>
       </div>
-      <div className="vertical-scroll scrollbar-track-rounded-full w-screen overflow-x-scroll flex scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-thumb-slate-700 scrollbar-thumb-rounded-full scrollbar-track-[rgba(0, 0, 0, 0)] ">
+      <div className="vertical-scroll  scrollbar-track-rounded-full w-screen overflow-x-scroll flex scrollbar-track-[rgba(0, 0, 0, 0)] ">
         {recommend.map((item: any, index) => {
           return (
 
