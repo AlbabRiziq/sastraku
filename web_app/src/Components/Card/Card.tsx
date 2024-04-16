ce6f10752e909e7a0c024c638326159452143092
AlbabRiziq <albabriziq123@gmail.com>
2024-04-17 04:17:17 +0700

    Refactor Card component to improve layout and remove unused code

--- web_app/src/Components/Card/Card.tsx
+++ web_app/src/Components/Card/Card.tsx
@@ -13,7 +13,7 @@ function Card({ title, author, desc, id, img }: CardProps) {
   // console.log(img);
 
   return (
-    <div className="card sm:h-60  w-96  lg:h-[500px] bg-base-100 shadow-xl bg-transparent border m-5 relative border-[#092635]">
+    <div className="card w-96 bg-base-100 shadow-xl bg-transparent border m-5 relative border-[#092635]">
       <div className="card-body p-7">
         <img src={img != null ? img : `https://placehold.co/600x300?text=${title}`.replace(/ /g, "+")} alt="" className="w-full rounded-2xl h-56 object-cover" />
         <div>
