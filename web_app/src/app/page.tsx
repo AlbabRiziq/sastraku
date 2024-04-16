3b00656e34ab1fe6d943c140a92987b0ccd248bf
AlbabRiziq <albabriziq123@gmail.com>
2024-04-17 02:29:13 +0700

    memodifikasi tampilan scroll bar padáhalaman utama

--- web_app/src/app/page.tsx
+++ web_app/src/app/page.tsx
@@ -8,7 +8,6 @@ import axios from "axios";
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
 
-// import 'swiper/css';
 import 'swiper/css/navigation';
 
 export default function Home() {
@@ -22,12 +21,9 @@ export default function Home() {
       }
     }).then((response) => {
       setData(response.data.data)
-      console.log(response.data.data);
-
 
     }).catch((error) => {
       console.log(error)
-
     })
   }, [])
 
@@ -37,27 +33,24 @@ export default function Home() {
       <div className="carousel w-full px-5">
         <h1 className="mt-5 font font-bold text-xl">REKOMENDASI</h1>
       </div>
-      <Swiper modules={[Navigation, Pagination, Scrollbar, A11y]}
-
-        navigation
-      >
+      <div className="vertical-scroll scrollbar-track-rounded-full w-screen overflow-x-scroll flex scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-thumb-slate-700 scrollbar-thumb-rounded-full scrollbar-track-[rgba(0, 0, 0, 0)] ">
         {data.map((item: any, index) => {
           return (
-            <SwiperSlide key={index} >
-              <div id={`slide${index}`} className="carousel-item relative w-full" key={index}>
-                <Card
-                  id={item.content_id}
-                  title={item.content_title}
-                  author={item.author}
-                  desc={item.content_description}
-                  img={item.cover_url}
-                />
 
-              </div>
-            </SwiperSlide>
+            <div id={`slide${index}`} className="carousel-item relative" key={index}>
+              <Card
+                id={item.content_id}
+                title={item.content_title}
+                author={item.author}
+                desc={item.content_description}
+                img={item.cover_url}
+              />
+
+            </div>
+
           )
         })}
-      </Swiper>
+      </div>
       <Navbar />
     </div>
   );
