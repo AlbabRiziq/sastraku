ba63d40f9e1050d35ec71be3eadb3c9ba86744b2
AlbabRiziq <albabriziq123@gmail.com>
2024-04-18 23:40:15 +0700

    Remove unused user_posts route

--- web_app/src/app/api/user_posts/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import dbConnect from "../../../lib/dbConnect";
-import Content from "../../../Models/Content";
-
-export async function GET(req: NextRequest, res: NextResponse) {
-    await dbConnect()
-
-
-} 
