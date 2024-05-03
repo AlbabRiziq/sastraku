ebcd53178b72dc826c12c36419bf3c1115dd5244
AlbabRiziq <albabriziq123@gmail.com>
2024-05-03 15:46:30 +0700

    an updates

---
+++ web_app/src/app/cari/layout.tsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+    title: "SASTRAKU | CARI ",
+    description: "Cari karya sastra yang kamu sukai disini",
+    kewords: "sastra, karya, puisi, cerpen, novel, sastraku, cari"
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <section>
+            {children}
+        </section>
+    );
+}
