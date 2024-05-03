4efd8d8b587f352be04ba593fdcff6a56cc84c08
AlbabRiziq <albabriziq123@gmail.com>
2024-05-03 15:23:18 +0700

    search

--- web_app/src/Components/Navbar/Navbar.tsx
+++ web_app/src/Components/Navbar/Navbar.tsx
@@ -2,30 +2,39 @@
 import Image from 'next/image';
 import homeImg from '../../assets/img/home.svg'
 import createImg from '../../assets/img/create.svg'
+import searchImg from '../../assets/img/search.svg'
 import profileImg from '../../assets/img/profile.svg'
+import Link from 'next/link';
 
 function Navbar() {
   return (
     <div className="fixed bottom-0 right-0 w-screen left-0 text-white flex items-center justify-center z-50">
       <div className="bg-[#092635] w-full flex items-center justify-evenly p-5 rounded-t-lg">
-        <a href="/">
+        <Link href="/">
           <Image
             alt='home'
             src={homeImg}
           />
-        </a>
-        <a href="/create">
+        </Link>
+        <Link href="/cari">
           <Image
-            alt='create'
+            alt='cari'
+            src={searchImg}
+          />
+        </Link>
+        <Link href="/create">
+          <Image
+            alt='buat'
             src={createImg}
           />
-        </a>
-        <a href="/profile">
+        </Link>
+        <Link href="/profile">
           <Image
-            alt='create'
+            alt='profil'
             src={profileImg}
           />
-        </a>
+        </Link>
+
       </div>
     </div>
   );
